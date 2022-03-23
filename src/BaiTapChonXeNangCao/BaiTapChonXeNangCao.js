173cf5cc780b5834ce6297eb803a7ba7b3eb6f34
Do Tung <54771496+dt-japanscript@users.noreply.github.com>
2022-03-23 20:57:34 +0900

    Create change wheel feature

--- src/BaiTapChonXeNangCao/BaiTapChonXeNangCao.js
+++ src/BaiTapChonXeNangCao/BaiTapChonXeNangCao.js
@@ -58,18 +58,28 @@ export default class BaiTapChonXeNangCao extends Component {
         })
     }
 
+    changeWheel = (newWheel) => {
+        //Tìm trong state hiện tại (this.state.carCurrent.wheels) 
+        let obWheel = this.state.carCurrent.wheels.find(item => item.idWheel === newWheel.idWheel);
+        if (obWheel !== -1) {
+            //lấy ra source hình ảnh từ this.state.carCurrent.wheels
+            this.setState({
+                carCurrent: { ...this.state.carCurrent, srcImg: obWheel.srcImg }
+            })
+        }
+    }
+
     renderWheels = () => {
         return dataWheels.map((item, index) => {
-            return (
-                <div className='row mt-1 border border-color-default m3' key={index}>
-                    <div className='col-2'>
-                        <img className='' style={{ width: '100%' }} src={item.img} alt='' />
-                    </div>
-                    <div className='col-10 d-flex flex-column align-items-center'>
-                        <span className='p-3 mt-5'>{item.title}</span>
-                    </div>
+            return <div style={{ cursor: 'pointer' }} onClick={() => { this.changeWheel(item) }} className="row mt-1 border border-color-default m-3" key={index}>
+                <div className="col-2">
+                    <img className="p-3" style={{ width: '100%' }} src={item.img} alt={index} />
                 </div>
-            )
+                <div className="col-10 d-flex flex-colum align-items-center" >
+                    <span className="p-3">{item.title}</span>
+
+                </div>
+            </div>
         })
     }
 
